97d5a7f30d15314bfe2686ed7777e2380dff456a
simi-ola <joybola94@gmail.com>
2022-12-16 20:13:51 -0800

    srdtfyg

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints stright line n times.
+ *
+ * @n: times stright line is printed
+ * Return: no return
+ */
+void print_line(int n)
+{
+	int i;
+
+	 for (1 = 0; 1 < n; i++)
+	 {
+		 _putchar(95);
+	 }
+	 _putchar('\n')
+}
