8bf31c40ead99d6f0f5e4770751b890381afc296
simi-ola <joybola94@gmail.com>
2022-12-16 17:09:52 -0800

    task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_N
+#define MAIN_N
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest = largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif
