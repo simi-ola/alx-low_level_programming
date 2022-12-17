38c00efc73bc025fa51e56235e80ed72690dac93
simi-ola <joybola94@gmail.com>
2022-12-16 20:36:49 -0800

    sdfghgv

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,7 +9,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void most_numbers(void);
 void print_line(int n);
-void print_digonal(int n);
+void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
