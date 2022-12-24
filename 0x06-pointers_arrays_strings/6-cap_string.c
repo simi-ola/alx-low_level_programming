ca89d02cfe19c0342dc9779f71dc3ddaf261bdc6
simi-ola <joybola94@gmail.com>
2022-12-23 18:09:15 -0800

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,8 +12,7 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
-
-				i++;
+			i++;
 		if (str[i - 1] == ' ' ||
 		    str[i - 1] == '\t' ||
 		    str[i - 1] == '\n' ||
