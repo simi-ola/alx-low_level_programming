5c1331db476cb1e9a0d94bfbd1d9cba2ce3b27e9
simi-ola <joybola94@gmail.com>
2022-12-23 17:41:52 -0800

    all tasks

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string with n
+ * @dest: copy to
+ * @src: copy from
+ * @n: numbers of char to be copied
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}
